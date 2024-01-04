ce272c4f39cc440bfab6b901588e533e22906a13
maga <vasnhe@mail.ru>
2024-01-04 19:35:17 +0300

    add script file

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// let storeName = 'Good';
+
+// const storeDescription = {
+// 	budget: 10000,
+// 	employees: ['maga', 'zalina', 'solih'],
+// 	products: {appel: 100,voda: 150},
+// 	open: true
+// };
+
+// console.log(obj.age);
+
+// alert('Hello')
+
+// const result = confirm('are you?');
+// console.log(result);
+
+// const answer = +prompt ('Вам есть 18?', '18');
+// console.log(answer + 5 );
+
+
+// let name = 'maga';
+
+// console.log('maga'+'zalina');
+
+// const isChecked = true,
+// 	  isClose = false;
+
+// console.log(isChecked || !isClose);
+
+// console.log(4 - +'5');
+
+let incr = 10,
+	decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(incr);
+console.log(decr);
+
